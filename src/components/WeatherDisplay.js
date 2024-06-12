b9035779cfd4300f5136eca114b60956a834b1b9
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-12 04:43:17 +0000

    test 1

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function WeatherDisplay({ weather }) {
     return (
-        <p>
-            <span style={{color: weather.temperature > 20 ? 'red': 'blue'}}>Temperature: {weather.temperature}</span> &nbsp;
-            Conditions: {weather.conditions}
-        </p>
+        <>
+            <p style={{color: weather.temperature > 20 ? 'red': 'blue'}}>Temperature: {weather.temperature}</p>
+            <p>Conditions: {weather.conditions}</p>
+        </>
     )
 }
 
